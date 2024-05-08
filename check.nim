0289ce47dff8a51e9da28e1a0c4595c254b93d5f
atunu <atunutemp1@gmail.com>
2024-05-08 21:07:43 +0600

    Add nph format and lint
    Signed-off-by: atunu <atunutemp1@gmail.com>

--- check.nim
+++ check.nim
@@ -4,23 +4,64 @@
 #
 # SPDX-License-Identifier: MIT
 
-import std/[osproc, terminal]
+import std/[osproc, strutils, terminal]
 
+# command names
+const reuse = "reuse"
+const nph = "nph"
+
+# format commands
+const nphFormatCmd = "nph ."
+
+# lint commands
 const reuseLintCmd = "reuse lint"
+const nphLintCmd = "nph --check ."
+
+proc runCmd(cmdName: string, cmdType: string, cmd: string): bool =
+  styledEcho fgYellow, "Running $# $#" % [cmdName, cmdType]
+  let (cmdOutput, cmdRes) = execCmdEx(cmd)
+  echo cmdOutput
+  result = cmdRes == 0
+  if result:
+    styledEcho fgGreen, "$# $# successful\n" % [cmdName, cmdType]
+  else:
+    styledEcho fgRed, "$# $# failed\n" % [cmdName, cmdType]
+
+proc format(): bool =
+  const cmdTypeFmt = "format"
+  # nph
+  let nphFmtRes = runCmd(nph, cmdTypeFmt, nphFormatCmd)
+  return nphFmtRes
 
 proc lint(): bool =
-  styledEcho fgYellow, "Running reuse lint"
-  let (reuseLintOutput, reuseLintRes) = execCmdEx(reuseLintCmd)
-  echo reuseLintOutput
-  result = reuseLintRes == 0
-  if result:
-    styledEcho fgGreen, "reuse lint successful\n"
-  else:
-    styledEcho fgRed, "reuse lint failed\n"
+  const cmdTypeLint = "lint"
+  # reuse
+  let reuseLintRes = runCmd(reuse, cmdTypeLint, reuseLintCmd)
+  # nph
+  let nphLintRes = runCmd(nph, cmdTypeLint, nphLintCmd)
+
+  return reuseLintRes and nphLintRes
 
 when isMainModule:
+  # format
+  styledEcho styleBright, "### Formatting..."
+  let formatRes = format()
+  if formatRes:
+    styledEcho styleBright, fgGreen, "Format Successful"
+  else:
+    styledEcho styleBright, fgRed, "Format Failure"
+  echo ""
+  # lint
+  styledEcho styleBright, "### Linting..."
   let lintResult = lint()
   if lintResult:
     styledEcho styleBright, fgGreen, "Lint Successful"
   else:
     styledEcho styleBright, fgRed, "Lint Failure"
+
+  echo "\n"
+  let totalRes = formatRes and lintResult
+  if totalRes:
+    styledEcho styleBright, fgGreen, "## Success"
+  else:
+    styledEcho styleBright, fgRed, "## Failure"
