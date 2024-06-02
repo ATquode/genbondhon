b3edcc50d311c68e1be898f07c4e77ccd8e36f29
atunu <atunutemp1@gmail.com>
2024-06-02 07:45:30 +0600

    Add bindingDir and wrapperName options
    for binding directory path and wrapper file name
    
    Signed-off-by: atunu <atunutemp1@gmail.com>

--- genbondhon/src/genbondhon.nim
+++ genbondhon/src/genbondhon.nim
@@ -7,14 +7,17 @@ const helpTxt =
 Generate bindings for Nim libraries to platform native technologies.
 
 Usage:
-  genbondhon [--verbose] <file>
+  genbondhon [-b <bindingDirName>] [-w <wrapperFileName>] [--verbose] <file>
+  genbondhon [--bindingDir <bindingDirName>] [--wrapperName <wrapperFileName>] [--verbose] <file>
   genbondhon --version
   genbondhon -h | --help
 
 Options:
-  --verbose     Show verbose output.
-  -h --help     Show help message.
-  --version     Show version
+  -b <bindingDirName>, --bindingDir <bindingDirName>          Binding Directory Path [default: bindings].
+  -w <wrapperFileName>, --wrapperName <wrapperFileName>       Wrapper File Name [default: binding_api].
+  --verbose                                                   Show verbose output.
+  -h --help                                                   Show help message.
+  --version                                                   Show version
 """
 
 import std/[parsecfg, paths, streams]
@@ -26,13 +29,15 @@ const version = "../genbondhon.nimble".staticRead.newStringStream.loadConfig.get
   "", "version"
 )
 
-proc generateBindings(verbose: bool, file: string) =
+proc generateBindings(
+    bindingDir: string, wrapperName: string, verbose: bool, file: string
+) =
   ## generates bindings for public APIs of the given nim file.
   showVerboseOutput = verbose
   moduleName = file.Path.lastPathPart.splitFile.name.string
   let publicAST = parsePublicAPIs(file.Path)
   let wrappedApis = translateToCompatibleWrapperApi(publicAST)
-  wrappedApis.generateWrapperFile("bindings".Path, "binding_api", file.Path)
+  wrappedApis.generateWrapperFile(bindingDir.Path, wrapperName, file.Path)
 
 when isMainModule:
   let args = docopt(helpTxt, version = version)
