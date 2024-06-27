67a8a46e286ffcca1b5786f89a314f7c96f90eaa
atunu <atunutemp1@gmail.com>
2024-06-28 00:32:57 +0600

    CLangGen mild refactor and documentation
    Signed-off-by: atunu <atunutemp1@gmail.com>

--- genbondhon/src/genbondhon/langs/c.nim
+++ genbondhon/src/genbondhon/langs/c.nim
@@ -11,19 +11,21 @@ type CLangGen = ref object of BaseLangGen
   headerFileName: Path
 
 proc newCLangGen*(bindingDir: Path): CLangGen =
+  ## `CLangGen` constructor
   result = CLangGen(
     langDir: bindingDir / "C".Path, headerFileName: moduleName.Path.addFileExt("h")
   )
   initBaseLangGen(result)
 
 func replaceType(nimCType: string): string =
+  ## Replaces Nim Compat Types to C Types
   nimCompatToCTypeTbl.getOrDefault(nimCType, nimCType)
 
-proc translateProc(node: PNode): string =
+func translateProc(node: PNode): string =
   let funcName = procName(node)
   let paramNode = procParamNode(node)
   var retType = "void"
-  var trParamList = newSeq[string]()
+  var trParamList: seq[string]
   if paramNode.isSome:
     let formalParamNode = paramNode.get()
     for i in 1 ..< formalParamNode.safeLen:
@@ -37,7 +39,7 @@ proc translateProc(node: PNode): string =
     &"""
 {retType.replaceType} {funcName}({trParamList.join(", ")});"""
 
-proc translateApi(api: PNode): string =
+func translateApi(api: PNode): string =
   case api.kind
   of nkProcDef, nkFuncDef, nkMethodDef:
     result = translateProc(api)
@@ -45,24 +47,12 @@ proc translateApi(api: PNode): string =
     result = "Cannot translate Api to C"
 
 func containsBool(apis: seq[PNode]): bool =
-  # for api in apis:
-  #   debugEcho api.procName
-  #   if api.procParamNode.isSome:
-  #     debugEcho api.procName & " has paramNode"
-  #     if api.procParamNode.get()[0].kind != nkEmpty:
-  #       if api.procParamNode.get[0].ident.s == "bool":
-  #         return true
-  #   else:
-  #     debugEcho api.procName & ": no paramNode"
-  # return false
   apis.anyIt(
     if it.procParamNode.isSome and it.procParamNode.get[0].kind != nkEmpty and
         it.procParamNode.get[0].ident.s == "bool": true else: false
   )
 
-proc generateCHeaderContent(
-    self: CLangGen, headerName: string, bindingAST: seq[PNode]
-): string =
+func generateCHeaderContent(headerName: string, bindingAST: seq[PNode]): string =
   let headerGuard = headerName.toUpperAscii & "_H"
   let optionalStdBoolH =
     if bindingAST.containsBool:
@@ -72,7 +62,7 @@ proc generateCHeaderContent(
 """
     else:
       ""
-  var cApis = newSeq[string]()
+  var cApis: seq[string]
   for api in bindingAST:
     let trApi = translateApi(api)
     cApis.add(trApi)
@@ -87,7 +77,7 @@ proc generateCHeaderContent(
 """
 
 proc generateCHeader(self: CLangGen, bindingAST: seq[PNode]) =
-  let content = self.generateCHeaderContent(moduleName, bindingAST)
+  let content = generateCHeaderContent(moduleName, bindingAST)
   if showVerboseOutput:
     styledEcho fgGreen, "C Header Content:"
     echo content
@@ -134,7 +124,7 @@ Copy the dll to pwd
     nim c -d:release --noMain:on --app:lib --outdir:{self.langDir.string} {self.bindingModuleFile.string}
 
 ### Usage
-Copy the lib binary and header file to your project.
+Copy the header file and lib binary to your project.
 
     cp {string self.langDir / self.headerFileName} {realTestDir.string}
 
@@ -168,5 +158,6 @@ Then [run & verify](#run--verify).
     echo result
 
 method generateBinding*(self: CLangGen, bindingAST: seq[PNode]) =
+  ## Generates binding & documentation for C
   self.generateCHeader(bindingAST)
   self.generateReadMe()
