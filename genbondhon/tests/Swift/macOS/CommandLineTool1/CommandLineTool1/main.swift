f76c3ad12576befc5e0afd3921378ab11a64e84a
atunu <atunutemp1@gmail.com>
2024-07-18 01:07:07 +0600

    Add Swift Command Line Tool test app
    Signed-off-by: atunu <atunutemp1@gmail.com>

---
+++ genbondhon/tests/Swift/macOS/CommandLineTool1/CommandLineTool1/main.swift
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2024 Rifat Hasan <atunutemp1@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+//
+//  main.swift
+//  CommandLineTool1
+//
+//  Created by Atunu on 7/17/24.
+//
+
+import Foundation
+
+NimMain()
+noop()
+extraNoOp()
+let a = constRet()
+print(a)
+let b = constRetBool()
+print(b)
+let c = constRetFloat()
+print(c)
+let d = constRetChar()
+print(d)
+let e = constRetStr()
+print(e)
+let f = constRetUnicodeStr()
+print(f)
+let g = addInt(a: 5, b: 3)
+print(g)
+printCond(a: g == 8)
+printCond(a: g != 8)
+let h = addDouble(a: 5.03, b: 3.05)
+print(h)
+let i = addFloat(a: 5.3, b: 3.5)
+print(i)
+takeChar(a: "a")
+printStr(a: "nim")
+printStr(a: "hello ñíℳ")
+let j = sayHello(name: "ñíℳ")
+print(j)
