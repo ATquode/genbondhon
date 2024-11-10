9ddcb367ebeb066c926a80c40d8305db8a58ad57
atunu <atunutemp1@gmail.com>
2024-11-10 14:35:11 +0600

    Replace assert with check in tParsePublicApi
    assert does not handle cleanup,
    in this case removing the binding directory.
    Besides, unittest module's check has nice failure
    info by default.
    
    Signed-off-by: atunu <atunutemp1@gmail.com>

--- genbondhon/tests/parsing/tParsePublicApi.nim
+++ genbondhon/tests/parsing/tParsePublicApi.nim
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-import std/[dirs, paths, strutils]
+import std/[dirs, paths, unittest]
 import
   ../../src/genbondhon/
     [currentconfig, parseutil, wrapperTranslator, wrapperGenerator, util]
@@ -14,24 +14,21 @@ moduleName = filePath.lastPathPart.splitFile.name.string
 bindingDirPath = "bindingTest".Path
 let wrappedFileName = "wrappedApi.nim"
 let publicApis = filePath.parsePublicAPIs()
-assert publicApis.len == publicApiCount,
-  "The number of public Apis ($#) don't match".format(publicApis.len)
+check publicApis.len == publicApiCount
 
 const expectedPublicProcs = ["noop", "extraNoOp"]
 for i in 0 ..< expectedPublicProcs.len:
-  assert publicApis[i].procName == expectedPublicProcs[i],
-    "$# doesn't match $#".format(publicApis[i].procName, expectedPublicProcs[i])
+  check publicApis[i].procName == expectedPublicProcs[i]
 
 let wrappedApis = publicApis.translateToCompatibleWrapperApi()
 let wrappedFile = wrappedApis.generateWrapperFile(wrappedFileName)
 let bindingApis = wrappedFile.parsePublicAPIs()
-assert bindingApis.len == publicApiCount + 1 # public APIs + NimMain()
+check bindingApis.len == publicApiCount + 1 # public APIs + NimMain()
 
 var expectedWrapperProcs = @["NimMain"]
 expectedWrapperProcs.add(expectedPublicProcs)
 
 for i in 0 ..< expectedWrapperProcs.len:
-  assert bindingApis[i].procName == expectedWrapperProcs[i],
-    "$# doesn't match $#".format(bindingApis[i].procName, expectedWrapperProcs[i])
+  check bindingApis[i].procName == expectedWrapperProcs[i]
 
 dirs.removeDir(bindingDirPath)
