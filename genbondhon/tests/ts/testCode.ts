1eb6edb4413d86a4a0bc5b3ebddd1cac92cb7a0d
atunu <atunutemp1@gmail.com>
2025-01-09 01:28:31 +0600

    Add example ts file using all public API
    Signed-off-by: atunu <atunutemp1@gmail.com>

---
+++ genbondhon/tests/ts/testCode.ts
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2025 Rifat Hasan <atunutemp1@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+import {
+  addDouble,
+  addFloat,
+  addIntNum,
+  constRet,
+  constRetBool,
+  constRetChar,
+  constRetFloat,
+  constRetStr,
+  constRetUnicodeStr,
+  extraNoOp,
+  noop,
+  printCond,
+  takeChar,
+  printStr,
+  sayHello,
+} from "./nomuna";
+
+noop();
+extraNoOp();
+let a = constRet();
+console.log(a);
+let b = constRetBool();
+console.log(b);
+let c = constRetFloat();
+console.log(c);
+let d = constRetChar();
+console.log(String.fromCharCode(parseInt(d)));
+let e = constRetStr();
+console.log(e);
+let f = constRetUnicodeStr();
+console.log(f);
+let g = addIntNum(5, 3);
+console.log(g);
+printCond(g == 8);
+printCond(g == 7);
+let h = addDouble(5.03, 3.05);
+console.log(h);
+let i = addFloat(5.3, 3.5);
+console.log(i);
+let j = "a".charCodeAt(0).toString();
+takeChar(j);
+printStr("nim");
+printStr("hello ñíℳ"); // Unicode
+let k = sayHello("ñíℳ");
+console.log(k);
