52b4fc49855bb8fa5d1da7707f9187d720c31de1
atunu <atunutemp1@gmail.com>
2025-01-13 00:31:28 +0600

    Add ESLint & Prettier to my-app1
    with Svelte support
    
    Signed-off-by: atunu <atunutemp1@gmail.com>

---
+++ genbondhon/tests/ts/my-app1/eslint.config.mjs
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Rifat Hasan <atunutemp1@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import svelteConfig from "./svelte.config.js";
+import * as svelteParser from "svelte-eslint-parser";
+import * as typescriptParser from "@typescript-eslint/parser";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginSvelte.configs["flat/recommended"],
+  eslintPluginSvelte.configs["flat/prettier"],
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: typescriptParser,
+        project: "./tsconfig.app.json",
+        extraFileExtensions: [".svelte"],
+        svelteConfig,
+      },
+    },
+  }
+);
